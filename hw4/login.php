1d2f23733f177bcb46ada70229fac1edfeea9bd9
Viet-Hungg <hungbkvn47@gmail.com>
2025-10-07 15:41:21 +0700

    w4

---
+++ hw4/login.php
@@ -0,0 +1,60 @@
+<?php
+include "db_connect.php";
+if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+
+    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result = mysqli_query($link, $query);
+
+    if (mysqli_num_rows($result) == 1) {
+        echo " Login successful!";
+    } else {
+        echo " Wrong information!";
+    }
+}
+if (isset($_POST['register'])) {
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+    $check = mysqli_query($link, "SELECT * FROM users WHERE username='$username'");
+    if (mysqli_num_rows($check) > 0) {
+        echo " Username already exists!";
+    } else {
+        $query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+        if (mysqli_query($link, $query)) {
+            echo " Registration successful! You can now log in.";
+        } else {
+            echo "❌ Error: " . mysqli_error($link);
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login & Register</title>
+</head>
+<body>
+    <h2>Login</h2>
+    <form action="" method="post">
+        <label>Username:</label>
+        <input type="text" name="username" required><br>
+        <label>Password:</label>
+        <input type="password" name="password" required><br>
+        <input type="submit" name="login" value="Login">
+    </form>
+
+    <hr>
+
+    <h2>Register</h2>
+    <form action="" method="post">
+        <label>Username:</label>
+        <input type="text" name="username" required><br>
+        <label>Password:</label>
+        <input type="password" name="password" required><br>
+        <input type="submit" name="register" value="Register">
+    </form>
+</body>
+</html>
